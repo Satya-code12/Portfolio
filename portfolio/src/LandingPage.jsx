04077ff18f8bc9bb083b5869d38d583cb5141557
Satyabrata Ojha <satyabrat637@gmail.com>
2024-07-21 22:30:44 +0530

    Style:Updated

--- portfolio/src/LandingPage.jsx
+++ portfolio/src/LandingPage.jsx
@@ -2,21 +2,24 @@ import React from "react";
 const MainPage =()=>{
   return(
     <>
-      <section>
-        <button> <span>#1.</span> About</button>
-        <button> <span>#2.</span> Work</button>
-        <button> <span>#3.</span> Contact</button>
-        <button> <span>#4.</span> Resume</button>
-      </section>
+      <section className="h-screen w-auto text-white">
+        <div className="h-[4rem] p-5 ">
+        <button className="ml-[16rem] h-9 w-[7rem] backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-700 rounded-lg"> <span>#1.</span> About</button>
+        <button className="ml-[9rem] h-9 w-[7rem] backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-700 rounded-lg"> <span>#2.</span> Work</button>
+        <button className="ml-[9rem] h-9 w-[7rem] backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-700 rounded-lg"> <span>#3.</span> Contact</button>
+        </div>
 
-      {/* <div>
+        <div >
         <h6>Hi, my name is,</h6>
         <h1>Satyabrata Ojha.</h1>
         <h3>A passionate Web Developer.</h3>
         <p> A passionate and dedicated front-end developer with a strong foundation in JavaScript and React.js. Over the years, I have honed my skills by working on various projects, including a Netflix Clone and a Tic Tac Toe game, which have allowed me to delve deep into the intricacies of web development.</p>
         <button> Check my resume</button>
         <button>Need a Frontend Developer?</button>
-      </div> */}
+      </div>
+      </section>
+
+      
     </>
   );
 }
